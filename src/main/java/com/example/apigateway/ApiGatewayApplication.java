b1ebf64c28c8436f14f2da79c51d825c4ab3133a
PaulW02 <pslwi@kth.se>
2024-01-08 14:03:46 +0100

    Cloud

--- src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -16,14 +16,14 @@ public class ApiGatewayApplication {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(r -> r.path("/user/**").uri("http://login-microservice-service:5001"))
-                .route(r -> r.path("/message/**").uri("http://message-microservice-service:5002"))
-                .route(r -> r.path("/patient/**").uri("http://patient-microservice-service:5003"))
-                .route(r -> r.path("/encounter/**").uri("http://patient-microservice-service:5003"))
-                .route(r -> r.path("/observation/**").uri("http://patient-microservice-service:5003"))
-                .route(r -> r.path("/condition/**").uri("http://patient-microservice-service:5003"))
-                .route(r -> r.path("/image/**").uri("http://image-microservice-service:5004"))
-                .route(r -> r.path("/search/**").uri("http://quarkus-microservice-service:8080"))
+                .route(r -> r.path("/user/**").uri("https://login-microservice.app.cloud.cbh.kth.se/"))
+                .route(r -> r.path("/message/**").uri("https://message-microservice.app.cloud.cbh.kth.se/"))
+                .route(r -> r.path("/patient/**").uri("https://patient-microservice.app.cloud.cbh.kth.se/"))
+                .route(r -> r.path("/encounter/**").uri("https://patient-microservice.app.cloud.cbh.kth.se/"))
+                .route(r -> r.path("/observation/**").uri("https://patient-microservice.app.cloud.cbh.kth.se/"))
+                .route(r -> r.path("/condition/**").uri("https://patient-microservice.app.cloud.cbh.kth.se/"))
+                .route(r -> r.path("/image/**").uri("https://image-microservice.app.cloud.cbh.kth.se/"))
+                .route(r -> r.path("/search/**").uri("https://quarkus-microservice.app.cloud.cbh.kth.se/"))
                 .build();
     }
 
